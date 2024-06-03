df30e33a367050437c5b5bf24ec5dd1d3169b8cc
Lucsducks <2021460003@unheval.pe>
2024-06-03 00:53:30 -0500

    arreglando img

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,38 @@
-import React from "react";
-
+import React, { useEffect, useRef } from "react";
 import styles from "./About.module.css";
 import { getImageUrl } from "../../utils";
 
 export const About = () => {
+  const aboutImageRef = useRef(null);
+
+  useEffect(() => {
+    const aboutImage = aboutImageRef.current;
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            aboutImage.classList.add(styles.visible);
+          } else {
+            aboutImage.classList.remove(styles.visible);
+          }
+        });
+      },
+      {
+        threshold: 0.1, 
+      }
+    );
+
+    if (aboutImage) {
+      observer.observe(aboutImage);
+    }
+
+    return () => {
+      if (aboutImage) {
+        observer.unobserve(aboutImage);
+      }
+    };
+  }, []);
+
   return (
     <section className={styles.container} id="about">
       <h2 className={styles.title}>About</h2>
@@ -12,6 +41,7 @@ export const About = () => {
           src={getImageUrl("about/aboutImage.png")}
           alt="Me sitting with a laptop"
           className={styles.aboutImage}
+          ref={aboutImageRef}
         />
         <ul className={styles.aboutItems}>
           <li className={styles.aboutItem}>
